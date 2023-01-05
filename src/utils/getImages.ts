0f30dd0616b04febeb50e34aa56a7a84ad928ef2
onadrog <sebastien.gordano@gmail.com>
2023-01-05 19:42:09 +0100

    Feat: Helper function `resolveLocalImage` to resolves images from assets directory from .astro & .md files.

---
+++ src/utils/getImages.ts
@@ -0,0 +1,17 @@
+interface Images {
+    src: string,
+    width: number,
+    height: number,
+    alt: string
+}
+
+const allImages = import.meta.glob("../assets/**/*{png,jgp,jpeg}")
+
+/*
+ *Import locale image from src/ folder as "../assets/***.png".
+ */
+export async function resolveLocalImage(path: string, alt: string = ""): Promise<Images> {
+    let img = await allImages[path]() as { default: Images };
+    img.default.alt = alt;
+    return img.default
+}
