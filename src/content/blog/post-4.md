e80d5b05c6fb901de1392ba1714f21c6ddc5547d
onadrog <sebastien.gordano@gmail.com>
2023-01-28 13:21:08 +0100

    Chore: format

--- src/content/blog/post-4.md
+++ src/content/blog/post-4.md
@@ -9,7 +9,6 @@ tags: ["astro", "successes"]
 
 This post should show up with my other blog posts, because `Astro.glob()` is returning a list of all my posts in order to create my list.
 
-
 ```js
 const a = 3;
 ```
--- src/pages/rss.xml.ts
+++ src/pages/rss.xml.ts
@@ -1,19 +1,19 @@
 import rss from "@astrojs/rss";
 import { getCollection } from "astro:content";
 
-import { site  as siteData} from "../data/site";
+import { site as siteData } from "../data/site";
 
-export async function get({site}: {site: string}) {
-	const posts = await getCollection("blog");
-	return rss({
-		title: siteData.rssTitle,
-		description: siteData.description,
-		site: site,
-		items: posts.map((post) => ({
-			link: `/blog/${post.slug}`,
-			title: post.data.title,
-			description: post.data.description,
-			pubDate: post.data.pubDate,
-		})),
-	});
+export async function get({ site }: { site: string }) {
+    const posts = await getCollection("blog");
+    return rss({
+        title: siteData.rssTitle,
+        description: siteData.description,
+        site: site,
+        items: posts.map((post) => ({
+            link: `/blog/${post.slug}`,
+            title: post.data.title,
+            description: post.data.description,
+            pubDate: post.data.pubDate,
+        })),
+    });
 }
